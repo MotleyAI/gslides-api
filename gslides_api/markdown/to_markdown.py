3e4e69d9a9bcd4ca71b9d27f6e65aaf2e6bcda7d
Egor Kraev <egor.kraev@gmail.com>
2025-12-14 12:55:02 +0100

    Remove to_markdown.py as superceded by agnostic logic

--- gslides_api/markdown/to_markdown.py
+++
@@ -1,8 +0,0 @@
-# This file previously contained text_elements_to_markdown() and related functions.
-# These have been replaced by the platform-agnostic IR system:
-#
-# - text_elements_to_ir() in gslides_api/agnostic/converters.py
-# - ir_to_markdown() in gslides_api/agnostic/ir_to_markdown.py
-#
-# The new system provides better run consolidation (prevents "doubled asterisks" bugs)
-# and cleaner separation of concerns via the intermediate representation (IR).
